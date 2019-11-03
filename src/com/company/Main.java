31a4f9466f5f3afa96991bf418757ed5175e134a
Dashtikh <mohammaddashti.kh@gmail.com>
2019-11-03 10:00:06 +0330

    code completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner1= new Scanner(System.in);
+        Scanner scanner2= new Scanner(System.in);
+        Scanner scanner3= new Scanner(System.in);
+        Scanner scanner4= new Scanner(System.in);
+        Scanner scanner5= new Scanner(System.in);
+        Info info= new Info();
+        System.out.println("enter your firsname : ");
+        info.setFname(scanner1.nextLine());
+        System.out.println("enter your last name : ");
+        info.setLname(scanner2.nextLine());
+        System.out.println("enter your address : ");
+        info.setAddress(scanner3.nextLine());
+        System.out.println("enter your password : ");
+        info.setPassword(scanner4.nextLine());
+        System.out.println("enter your email address : ");
+        info.setEmail(scanner5.nextLine());
+        System.out.println(info.getFname());
+        System.out.println(info.getLname());
+        System.out.println(info.getAddress());
+        System.out.println(info.getPassword());
+        System.out.println(info.getEmail());
+
+
     }
 }
